0b5c24fbf60cb03abc257674366ae06e994446a6
alexthegreat194 <alexander.gentry@aol.com>
2022-10-03 23:15:52 -0700

    signup

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ app.get('/login', (req, res) => {
     res.render('login');
 });
 
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
